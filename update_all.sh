4e8f9003ee555db8117d9e82f76fa5dd9a007cd2
Torben <59419684+entorb@users.noreply.github.com>
2020-03-24 06:27:47 +0100

    update 24.04.

--- update_all.sh
+++ update_all.sh
@@ -14,4 +14,8 @@ cd ..
 rsync -rvhu --delete --delete-excluded plots-gnuplot/*.png entorb@entorb.net:html/COVID-19-coronavirus/plots-gnuplot/
 rsync -rvhu --delete --delete-excluded plots-excel/*.png entorb@entorb.net:html/COVID-19-coronavirus/plots-excel/
 
+echo Date DE States: `tail -1 data/de-states-latest.tsv | cut -f5`
+echo Date World: `tail -1 data/countries-latest-selected.tsv | cut -f2
+`
+
 # cd dir_old
