81e68f3e0fb476464b1216b6de951d9e094f558a
Torben <59419684+entorb@users.noreply.github.com>
2020-03-25 03:02:12 +0100

    small stuff

--- update_all.sh
+++ update_all.sh
@@ -15,7 +15,10 @@ rsync -rvhu --delete --delete-excluded plots-gnuplot/*.png entorb@entorb.net:htm
 rsync -rvhu --delete --delete-excluded plots-excel/*.png entorb@entorb.net:html/COVID-19-coronavirus/plots-excel/
 
 echo Date DE States: `tail -1 data/de-states-latest.tsv | cut -f5`
-echo Date World: `tail -1 data/countries-latest-selected.tsv | cut -f2
-`
+echo Date World: `tail -1 data/countries-latest-selected.tsv | cut -f2`
+
+# git add .
+# git commit -m "update 24.04."
+# git push
 
 # cd dir_old
