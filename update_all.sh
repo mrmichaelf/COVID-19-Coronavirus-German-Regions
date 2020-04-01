3b09533adc0bcaf54fe3b9f5745f0a0edd0bb27b
Torben <59419684+entorb@users.noreply.github.com>
2020-04-01 07:45:24 +0200

    update

--- update_all.sh
+++ update_all.sh
@@ -4,7 +4,7 @@
 # dir_of_this_script="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # cd dir_of_this_script
 
-python fetch-country-data.py
+python fetch-int-country-data.py
 python fetch-de-states-data.py
 rm plots-gnuplot/*.png
 cd scripts-gnuplot
@@ -27,8 +27,8 @@ read ok
 rsync -rvhu --delete --delete-excluded plots-gnuplot/*.png entorb@entorb.net:html/COVID-19-coronavirus/plots-gnuplot/
 rsync -rvhu --delete --delete-excluded plots-excel/*.png entorb@entorb.net:html/COVID-19-coronavirus/plots-excel/
 
-echo Date DE States: `tail -1 data/de-states-latest.tsv | cut -f5`
-echo Date World: `tail -1 data/countries-latest-selected.tsv | cut -f2`
+echo Date DE-States: `tail -1 data/de-states/de-states-latest.tsv | cut -f5`
+echo Date Int-Countries: `tail -1 data/countries-latest-selected.tsv | cut -f2`
 
 
 # git add .
