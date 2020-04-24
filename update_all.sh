191a18b4ec7ae5ba7524e314e74a493fa7a78a78
Torben <59419684+entorb@users.noreply.github.com>
2020-04-24 13:37:33 +0200

    modify update script

--- update_all.sh
+++ update_all.sh
@@ -7,54 +7,39 @@
 
 wget -q -O index-online.html https://entorb.net/COVID-19-coronavirus/index.html 
 
+# fetching and generating new data
+python fetch-de-states-data.py
 python fetch-de-divi-V2.py
 python fetch-de-districts.py
-
 python fetch-int-country-data.py
-echo Date Int-Countries: `tail -1 data/int/countries-latest-selected.tsv | cut -f2`
 
-# echo "shall we continue?"
-# read ok
-
-rsync -rvhu --delete --delete-excluded data/* entorb@entorb.net:html/COVID-19-coronavirus/data/
-rsync -rvhu --delete --delete-excluded plots-gnuplot/* entorb@entorb.net:html/COVID-19-coronavirus/plots-gnuplot/
-
+# plotting
 rm plots-gnuplot/int/*.png
 cd scripts-gnuplot
+gnuplot plot-de.gp
 gnuplot plot-countries.gp
 cd ..
 
-
-python fetch-de-states-data.py
-echo Date DE-States: `tail -1 data/de-states/de-states-latest.tsv | cut -f5`
-# echo "shall we continue?"
-# read ok
-
+# uploading
 rsync -rvhu --delete --delete-excluded data/* entorb@entorb.net:html/COVID-19-coronavirus/data/
+rsync -rvhu --delete --delete-excluded plots-gnuplot/* entorb@entorb.net:html/COVID-19-coronavirus/plots-gnuplot/
 
-rm plots-gnuplot/de-states/*.png
-cd scripts-gnuplot
-gnuplot plot-de.gp
-cd ..
+# reporting
+echo Date Int-Countries: `tail -1 data/int/countries-latest-selected.tsv | cut -f2`
+echo Date DE-States: `tail -1 data/de-states/de-states-latest.tsv | cut -f5`
 
-# firefox index-online.html
-
-echo "Check local html. Enter to continue with upoading, CTRG+C to cancel"
+echo "Check local html. Enter to close, CTRG+C to cancel"
 read ok
 
 
 # python fetch-de-districts.py
 # python fetch-de-divi.py
 
-rsync -rvhu --delete --delete-excluded plots-gnuplot/* entorb@entorb.net:html/COVID-19-coronavirus/plots-gnuplot/
-# rsync -rvhu --delete --delete-excluded plots-excel/* entorb@entorb.net:html/COVID-19-coronavirus/plots-excel/
-
-rsync -rvhu --delete --delete-excluded data/* entorb@entorb.net:html/COVID-19-coronavirus/data/
-
-# rsync -rvhu results-de-districts.html entorb@entorb.net:html/COVID-19-coronavirus/results-de-districts.html
 
 # git add .
-# git commit -m "update 24.04."
+# git add data/*
+# git add plot-gnuplot/*
+# git commit -m "update"
 # git push
 
 # cd dir_old
