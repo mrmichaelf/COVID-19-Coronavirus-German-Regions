299d1d8098327eff807a527bf1b6c2bf5c34e4e1
Torben <59419684+entorb@users.noreply.github.com>
2020-04-27 05:03:52 +0200

    publishing map de districts

---
+++ gen-map-de-districts.py
@@ -0,0 +1,162 @@
+# Based on
+# https://raw.githubusercontent.com/ythlev/covid-19/master/run.py
+# by Chang Chia-huan
+
+import glob
+import argparse
+import pathlib
+import json
+import csv
+import io
+import urllib.request
+import urllib.parse
+import math
+import statistics
+import datetime
+import re
+
+# my helper modules
+import helper
+
+unit = 1000000
+
+# https://www.w3schools.com/colors/colors_picker.asp
+d_color_scales = {
+    'template': [
+        "#fee5d9", "#fcbba1", "#fc9272", "#fb6a4a", "#de2d26", "#a50f15"
+    ],
+    'blue': [
+        '#e6e6ff',
+        '#b3b3ff',
+        '#8080ff',
+        '#4d4dff',
+        '#1a1aff',
+        '#0000e6'
+    ],
+    'red':
+    [
+        '#ffe6e6',
+        '#ffb3b3',
+        '#ff8080',
+        '#ff4d4d',
+        '#ff1a1a',
+        '#cc0000'
+    ]
+}
+
+d_all_date_data = {}
+for f in glob.glob('data/de-districts/de-distict_timeseries-*.json'):
+    lk_id = int(re.search('^.*de-distict_timeseries\-(\d+)\.json$', f).group(1))
+    l = helper.read_json_file(f)
+    for d in l:
+        date = d['Date']
+        if date[0:7] == '2020-01' or date[0:7] == '2020-02':
+            continue
+        if not d['Date'] in d_all_date_data:
+            d_all_date_data[d['Date']] = {}
+        del d['Timestamp'], d['Date'], d['Days_Past'], d['Days_Since_2nd_Death'], d['Cases_Change_Factor'], d['Deaths_Change_Factor']
+        d_all_date_data[date][lk_id] = d
+del f, d, l
+
+# check if last date has as many values as the 2nd last, of not drop it
+dates = sorted(d_all_date_data.keys())
+if len(d_all_date_data[dates[-1]]) != len(d_all_date_data[dates[-2]]):
+    print(
+        f'{len(d_all_date_data[dates[-1]])} != {len(d_all_date_data[dates[-2]])}')
+    del d_all_date_data[dates[-1]]
+del dates
+
+
+property_to_plot = 'Deaths_Last_Week_Per_Million'
+for property_to_plot in ('Cases_Last_Week_Per_Million', 'Deaths_Last_Week_Per_Million'):
+
+    if property_to_plot == 'Cases_Last_Week_Per_Million':
+        meta = {"colour": d_color_scales['blue']}
+    elif property_to_plot == 'Deaths_Last_Week_Per_Million':
+        meta = {"colour": d_color_scales['red']}
+
+    values = []
+    # collect all values for autoscaling
+    for date_str, l_distritcs in d_all_date_data.items():
+        for lk_id, d in l_distritcs.items():
+            values.append(d[property_to_plot])
+    del d, l_distritcs, lk_id
+
+    # generate color scale range
+    q = statistics.quantiles(values, n=100, method="inclusive")
+    step = math.sqrt(statistics.mean(values) - q[0]) / 3
+    threshold = [0, 0, 0, 0, 0, 0]
+    for i in range(1, 6):
+        threshold[i] = math.pow(i * step, 2) + q[0]
+    del q, step, i
+
+    with open('maps/template_de-districts.svg', mode="r", newline="", encoding="utf-8") as file_in:
+        # plot loop for each date
+        # date_str = '2020-04-24'
+        # l_distritcs = d_all_date_data[date_str]
+        for date_str, l_distritcs in d_all_date_data.items():
+            file_in.seek(0, 0)  # reset file pointer
+            main = {}
+            for lk_id, d in l_distritcs.items():
+                area = lk_id
+                pcapita = d[property_to_plot]
+                main[area] = {'pcapita': pcapita}
+
+            with open(f'maps/out/de-districts/{property_to_plot}-{date_str}.svg', mode="w", newline="", encoding="utf-8") as file_out:
+                if threshold[5] >= 10000:
+                    num = "{:_.0f}"
+                elif threshold[1] >= 10:
+                    num = "{:.0f}"
+                else:
+                    num = "{:.2f}"
+
+                for row in file_in:
+                    written = False
+                    for area in main:
+                        if row.find('id="{}"'.format(area)) > -1:
+                            i = 0
+                            while i < 5:
+                                if main[area]["pcapita"] >= threshold[i + 1]:
+                                    i += 1
+                                else:
+                                    break
+                            file_out.write(row.replace('id="{}"'.format(
+                                area), 'style="fill:{}"'.format(meta["colour"][i])))
+                            written = True
+                            break
+                    if written == False:
+                        if row.find('>Date') > -1:
+                            file_out.write(row.replace(
+                                'Date', date_str))
+                        elif row.find('>level') > -1:
+                            for i in range(6):
+                                if row.find('level{}'.format(i)) > -1:
+                                    if i == 0:
+                                        file_out.write(row.replace('level{}'.format(
+                                            i), "&lt; " + num.format(threshold[1]).replace("_", "&#8201;")))
+                                    else:
+                                        file_out.write(row.replace('level{}'.format(
+                                            i), "≥ " + num.format(threshold[i]).replace("_", "&#8201;")))
+                        elif row.find('<path fill="#') > -1:
+                            s = row
+                            for i in range(6):
+                                s = s.replace(
+                                    d_color_scales["template"][i], meta["colour"][i])
+                            file_out.write(s)
+                        # elif row.find('!!!TYPE!!!') > -1:
+                        #     if property_to_plot == 'Cases_Last_Week_Per_Million':
+                        #         file_out.write(row.replace(
+                        #             '!!!TYPE!!!', 'Infizierte'))
+                        #     elif property_to_plot == 'Deaths_Last_Week_Per_Million':
+                        #         file_out.write(row.replace(
+                        #             '!!!TYPE!!!', 'Verstorbene'))
+                        else:
+                            file_out.write(row)
+        # break
+
+
+# from svglib.svglib import svg2rlg
+# from reportlab.graphics import renderPDF, renderPM
+# drawing = svg2rlg("file.svg")
+# ' renderPDF.drawToFile(drawing, "file.pdf")
+# renderPM.drawToFile(drawing, "file.png", fmt="PNG")
--- maps/svg2pngs.cmd
+++ maps/svg2pngs.cmd
@@ -1,3 +1,5 @@
 rem magick convert -size 480x de-districts-2020-01-28.svg -resize 480x de-districts-2020-01-28.png
 
-magick convert -delay 150x1000 -size 480x out/*.svg -coalesce -fuzz 2% +dither -resize 480x -layers Optimize out/ani.gif
+magick convert -delay 150x1000 -size 480x out/de-districts/Cases_Last_Week_Per_Million-*.svg -coalesce -fuzz 2% +dither -resize 480x -layers Optimize out/de-districts-Cases_Last_Week_Per_Million.gif
+
+magick convert -delay 150x1000 -size 480x out/de-districts/Deaths_Last_Week_Per_Million-*.svg -coalesce -fuzz 2% +dither -resize 480x -layers Optimize out/de-districts-Deaths_Last_Week_Per_Million.gif
