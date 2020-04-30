2065078a9ca167ab4162e37b60f0011e1c91a27a
Torben Menke <59419684+entorb@users.noreply.github.com>
2020-04-30 11:17:16 +0200

    shebang

--- data/upload-to-entorb.sh
+++ data/upload-to-entorb.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 rsync -rvhu --delete --delete-excluded ../data/* entorb@entorb.net:html/COVID-19-coronavirus/data/
--- scripts-gnuplot/upload-to-entorb.sh
+++ scripts-gnuplot/upload-to-entorb.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 # rsync -rvhu --delete --delete-excluded ../plots-excel/* entorb@entorb.net:html/COVID-19-coronavirus/plots-excel/
 rsync -rvhu --delete --delete-excluded ../plots-gnuplot/* entorb@entorb.net:html/COVID-19-coronavirus/plots-gnuplot/
