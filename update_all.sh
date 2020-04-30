eae6d53982db132b938e201e225f104d8c9e36eb
Torben Menke <59419684+entorb@users.noreply.github.com>
2020-04-30 10:33:53 +0200

    update

--- update_all.sh
+++ update_all.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+echo === git pull
+git pull
 
-# fetch current index.html
+echo === fetching index.html -> index-online.html
 wget -q -O index-online.html https://entorb.net/COVID-19-coronavirus/index.html 
 
-# fetching and generating new data
+echo === fetching and generating new data
 python3 fetch-de-states-data.py
 python3 fetch-de-divi-V2.py
 python3 fetch-de-districts.py
@@ -12,29 +14,31 @@ python3 fetch-int-country-data.py
 # IDEA: run in background processes via appending &
 
 
-# plotting
+echo === plotting
 rm plots-gnuplot/*/*.png
 cd scripts-gnuplot
 gnuplot all.gp
 bash upload-to-entorb.sh
 cd ..
 
-# uploading data # (after plotting because gnuplot generates 2 data files as well)
+echo  === uploading data
+# (after plotting because gnuplot generates 2 data files as well)
 cd data
 bash upload-to-entorb.sh
 cd ..
 
-# reporting
+echo  === date of data
 echo Date Int-Countries: `tail -1 data/int/countries-latest-selected.tsv | cut -f2`
 echo Date DE-States: `tail -1 data/de-states/de-states-latest.tsv | cut -f5`
 
-echo "Check local html. Enter to close, CTRG+C to cancel"
+echo === Check local html. Enter to close, CTRG+C to cancel
 read ok
 
+echo  === git: add and commit
 # git add .
 git add data/*
 # git add plot-gnuplot/*
-# git commit -m "update"
+git commit -m "update"
 # git push
 
 # cd dir_old
