92d83d63ccaf7844c42cf1853bb37ea4aa9d86e5
Torben Menke <59419684+entorb@users.noreply.github.com>
2020-05-04 07:12:52 +0200

    update multithreading

--- gen-map-de-districts.py
+++ gen-map-de-districts.py
@@ -190,7 +190,7 @@ for property_to_plot in ('Cases_Last_Week_Per_Million', 'Deaths_Last_Week_Per_Mi
                             file_out.write(row)
         # break
     l_subprocesses = []
-    for month in ('2020-03', '2020-04'):
+    for month in ('2020-03', '2020-04', '2020-05'):
         # convert -size 480x maps/out/de-districts/Cases_Last_Week_Per_Million-2020-03*.svg -resize 480x -coalesce -fuzz 2% +dither -layers Optimize maps/out/de-districts/Cases_Last_Week_Per_Million-2020-03.gif
         l_imagemagick_parameters = [
             '-size', '480x', f'maps/out/de-districts/{property_to_plot}-{month}*.svg', '-resize', '480x', '-coalesce', '-fuzz', '2%', '+dither', '-layers', 'Optimize', f'maps/out/de-districts/{property_to_plot}-{month}.gif']
---
+++ update_all_multithreading.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+echo === git pull
+git pull
+
+echo === fetching index.html -> index-online.html
+wget -q -O index-online.html https://entorb.net/COVID-19-coronavirus/index.html 
+
+echo === fetching and generating new data
+# echo ==== de-divi
+python3 fetch-de-divi-V2.py &
+# echo ==== de-states
+python3 fetch-de-states-data.py &
+# echo ==== de-districts
+python3 fetch-de-districts.py && python3 gen-map-de-districts.py &
+# echo ==== countries
+python3 fetch-int-country-data.py &
+
+# wait until all child processes are done
+wait
+
+# IDEA: run in background processes via appending '&'. Problem 1: how to know when ready for plotting? Problem 2: Errors are no longer displayed at the terminal
+
+
+echo === plotting + uploading plots
+rm plots-gnuplot/*/*.png
+cd scripts-gnuplot
+gnuplot all.gp
+bash upload-to-entorb.sh
+cd ..
+
+echo  === uploading data
+# (after plotting because gnuplot generates 2 data files as well)
+cd data
+bash upload-to-entorb.sh
+cd ..
+
+echo  === uploading maps
+cd maps
+bash upload-to-entorb.sh
+cd ..
+
+
+echo  === date of data
+echo Date Int-Countries: `tail -1 data/int/countries-latest-selected.tsv | cut -f2`
+echo Date DE-States: `tail -1 data/de-states/de-states-latest.tsv | cut -f5`
+
+echo === Check local html. Enter to copy and commit, CTRG+C to cancel
+read ok
+
+#rsync -rvhu --delete --delete-excluded ../plots-gnuplot/* entorb@entorb.net:html/COVID-19-coronavirus/plots-gnuplot/
+
+scp index.html entorb@entorb.net:html/COVID-19-coronavirus/
+
+
+echo  === git: add and commit
+git add data/*
+git commit -m "update"
+git push
+
