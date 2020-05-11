8f641f99f0d72f1922a3f3e378698bef350a1370
Torben Menke <59419684+entorb@users.noreply.github.com>
2020-05-11 06:35:45 +0200

    small change

--- update_all_multithreading.sh
+++ update_all_multithreading.sh
@@ -43,6 +43,7 @@ cd ..
 echo  === date of data
 echo Date Int-Countries: `tail -1 data/int/countries-latest-selected.tsv | cut -f2`
 echo Date DE-States: `tail -1 data/de-states/de-states-latest.tsv | cut -f5`
+echo Date DE-Erlangen: `tail -1 data/de-districts/de-district_timeseries-09562.tsv | cut -f2`
 
 # echo === Check local html. Enter to copy and commit, CTRG+C to cancel
 # read ok
