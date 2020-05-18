e51bcf8f99ddfae86e2985843279ff8e78079fcf
Torben Menke <59419684+entorb@users.noreply.github.com>
2020-05-18 22:08:39 +0200

    test reset charts

--- upload-index-to-entorb.sh
+++ upload-index-to-entorb.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+rsync -rvhu --delete --delete-excluded --no-perms index-reset.html entorb@entorb.net:html/COVID-19-coronavirus/
+rsync -rvhu --delete --delete-excluded --no-perms eCharts/myHelper-reset.js entorb@entorb.net:html/COVID-19-coronavirus/eCharts/
+
 rsync -rvhu --delete --delete-excluded --no-perms index.html entorb@entorb.net:html/COVID-19-coronavirus/
 rsync -rvhu --delete --delete-excluded --no-perms eCharts/myHelper.js entorb@entorb.net:html/COVID-19-coronavirus/eCharts/
 
 
 rsync -rvhu --delete --delete-excluded --no-perms index-nav*.html entorb@entorb.net:html/COVID-19-coronavirus/
+
