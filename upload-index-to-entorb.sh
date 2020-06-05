d56436deae86500a264c4b1f87c2b659966754b0
Torben Menke <59419684+entorb@users.noreply.github.com>
2020-06-05 05:09:30 +0200

    small stuff

--- upload-index-to-entorb.sh
+++ upload-index-to-entorb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-rsync -rvhu --delete --delete-excluded --no-perms index-url.html entorb@entorb.net:html/COVID-19-coronavirus/
-rsync -rvhu --delete --delete-excluded --no-perms eCharts/myHelper-url.js entorb@entorb.net:html/COVID-19-coronavirus/eCharts/
+# rsync -rvhu --delete --delete-excluded --no-perms index-url.html entorb@entorb.net:html/COVID-19-coronavirus/
+# rsync -rvhu --delete --delete-excluded --no-perms eCharts/myHelper-url.js entorb@entorb.net:html/COVID-19-coronavirus/eCharts/
 
 rsync -rvhu --delete --delete-excluded --no-perms index.html entorb@entorb.net:html/COVID-19-coronavirus/
 rsync -rvhu --delete --delete-excluded --no-perms eCharts/myHelper.js entorb@entorb.net:html/COVID-19-coronavirus/eCharts/
 
 
-rsync -rvhu --delete --delete-excluded --no-perms index-nav*.html entorb@entorb.net:html/COVID-19-coronavirus/
+# rsync -rvhu --delete --delete-excluded --no-perms index-nav*.html entorb@entorb.net:html/COVID-19-coronavirus/
 
