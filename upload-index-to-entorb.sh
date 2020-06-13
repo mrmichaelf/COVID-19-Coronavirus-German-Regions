d2958305722d397c983e59b8bdaaef5e4e840991
Torben Menke <59419684+entorb@users.noreply.github.com>
2020-06-13 23:08:58 +0200

    archiving of exp growth

--- upload-index-to-entorb.sh
+++ upload-index-to-entorb.sh
@@ -4,6 +4,7 @@
 # rsync -rvhu --delete --delete-excluded --no-perms eCharts/myHelper-url.js entorb@entorb.net:html/COVID-19-coronavirus/eCharts/
 
 rsync -rvhu --delete --delete-excluded --no-perms index.html entorb@entorb.net:html/COVID-19-coronavirus/
+rsync -rvhu --delete --delete-excluded --no-perms expGrowth.html entorb@entorb.net:html/COVID-19-coronavirus/
 rsync -rvhu --delete --delete-excluded --no-perms eCharts/myHelper.js entorb@entorb.net:html/COVID-19-coronavirus/eCharts/
 
 
