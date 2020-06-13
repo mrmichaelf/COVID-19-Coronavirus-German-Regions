f63053551a64f94065f0ce65cf68db9e3e049f27
Torben Menke <59419684+entorb@users.noreply.github.com>
2020-06-13 21:43:25 +0200

    start of alerter

---
+++ alerter/db_create.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+# TODO: move away from web dir
+pathToDb = 'alerter/covid-19-alert.db'
+con = sqlite3.connect(pathToDb)
+# con = sqlite3.connect(":memory:")
+# con.row_factory = sqlite3.Row  # allows for access via row["name"]
+cur = con.cursor()
+
+
+def create_table():
+    cur.execute("""
+      CREATE TABLE alerts (mail text, activated int, hash text, threshhold int)
+      """
+                )
+
+
+def test_insert():
+    myTuple = ("test@entorb.net", 1, "<hash>", 250)
+    cur.execute(f"INSERT INTO alerts VALUES (?,?,?,?)", myTuple)
+    con.commit()
+
+
+def test_select():
+    for row in cur.execute("SELECT * FROM alerts"):
+        print(row)
+    print("We now have %s rows in the DB table" %
+          cur.execute("SELECT count(*) FROM alerts").fetchone()[0])
+
+
+# create_table()
+test_insert()
+test_select()
+
+
+cur.close()
+con.close()
