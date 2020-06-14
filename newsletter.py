4787910923628b32285801a1bbac9fa27a8d2876
Torben Menke <59419684+entorb@users.noreply.github.com>
2020-06-14 08:05:42 +0200

    newsletter

--- newsletter.py
+++ newsletter.py
@@ -1,22 +1,26 @@
 import os
 import sqlite3
+import json
 
 # my helper modules
-import helper
+# import helper
 
 # check I runnung on entorb.net webserver
 if os.path.isdir("/home/entorb/data-web-pages/covid-19"):
     pathToDb = '/home/entorb/data-web-pages/covid-19/newsletter.db'
+    pathToData = '/home/entorb/html/COVID-19-coronavirus/data/de-districts/de-districts-results.json'
 else:
     pathToDb = 'cache/newsletter.db'
+    pathToData = 'data/de-districts/de-districts-results.json'
 
 con = sqlite3.connect(pathToDb)
 con.row_factory = sqlite3.Row  # allows for access via row["name"]
 cur = con.cursor()
 
 # load latest data
-d_districts_latest = helper.read_json_file(
-    "data/de-districts/de-districts-results.json")
+d_districts_latest = {}
+with open(pathToData, mode='r', encoding='utf-8') as fh:
+    d_districts_latest = json.load(fh)
 
 # loop over subscriptions
 for row in cur.execute("SELECT email, threshhold, regions FROM newsletter WHERE activated = 1"):
@@ -38,6 +42,6 @@ for row in cur.execute("SELECT email, threshhold, regions FROM newsletter WHERE 
         print("%3d (%3d)    : %s" % (
             d["Cases_Last_Week_Per_Million"], d["Cases_Last_Week"], d["Landkreis"]))
     # table footer
-    print("* Neu-Infektionen letzte Woche pro Millionen Einwohner (Neu-Infektionen letzte Woche absolut)")
+    print("* Neu-Infektionen letzte Woche pro Millionen Einwohner und Neu-Infektionen letzte Woche absolut")
     print(
         f"Chart ULR = https://entorb.net/COVID-19-coronavirus/?yAxis=Cases_Last_Week_Per_Million&DeDistricts={s_this_regions}#DeDistrictChart")
